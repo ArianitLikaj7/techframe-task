3f54877c095b8824f1a88eac23a9660d32cab935
Arianit Likaj <arianitlikaj46@gmail.com>
2024-05-25 14:53:34 +0200

    first-commit

---
+++ src/test/java/com/arianit/tech_frame_task/TechFrameTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arianit.tech_frame_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechFrameTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
